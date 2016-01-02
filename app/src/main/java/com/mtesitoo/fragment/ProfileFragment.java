ed6736adc5a2002f1b69eb343e057fd6a35e487b
jnwu <nan.wu.eng@gmail.com>
2016-01-01 23:29:43 -0800

    Updated profile fragment

--- app/src/main/java/com/mtesitoo/fragment/ProfileFragment.java
+++ app/src/main/java/com/mtesitoo/fragment/ProfileFragment.java
@@ -1,9 +1,28 @@
 package com.mtesitoo.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mtesitoo.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Nan on 12/30/2015.
  */
 public class ProfileFragment extends Fragment {
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_home_fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
 }
