e096f99bf05c2d6cbf9e3f543d0a26950b21c62e
Eduardo Díaz <eduardo.destrada@gmail.com>
2017-10-22 18:14:24 +0100

    Basic product picture fragment

--- app/src/main/java/com/mtesitoo/adapter/AddProductPicturesAdapter.java
+++ app/src/main/java/com/mtesitoo/adapter/AddProductPicturesAdapter.java
@@ -1,5 +1,7 @@
 package com.mtesitoo.adapter;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,8 +9,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.mtesitoo.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by eduardodiaz on 20/10/2017.
@@ -16,36 +23,100 @@ import butterknife.BindView;
 
 public class AddProductPicturesAdapter extends RecyclerView.Adapter<AddProductPicturesAdapter.ViewHolder> {
 
+    private List<Uri> pictures;
+    private Callback callback;
+    private Context context;
+
+    private final int MAX_PICTURES = 4;
+
+    public AddProductPicturesAdapter(Context context, Callback callback) {
+
+        this.pictures = new ArrayList<>(MAX_PICTURES);
+        this.callback = callback;
+        this.context = context;
+        Picasso.with(context).setLoggingEnabled(true);
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.add_product_list_item, parent, false);
 
-        return new ViewHolder(v);
+        return new ViewHolder(v, callback);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
+        if (getItemCount() == 1 || (pictures.size() != MAX_PICTURES && position == pictures.size())) {
+            holder.bind(context, position, Uri.EMPTY);
+            return;
+        }
+        holder.bind(context, position, pictures.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 1;
+        return pictures.size() < MAX_PICTURES ? 1 + pictures.size() : pictures.size();
+    }
+
+    public void insertPicture(int position, Uri url) {
+        if (pictures.size() == MAX_PICTURES - 1 && position == MAX_PICTURES - 1) {
+            pictures.add(position, url);
+            notifyDataSetChanged();
+            return;
+        }
+
+        pictures.add(position, url);
+        notifyDataSetChanged();
+        //notifyItemInserted(position);
+    }
+
+    public void updatePicture(int position, Uri url) {
+        pictures.set(position, url);
+        notifyItemChanged(position);
+    }
+
+    public void removePicture(int position) {
+        pictures.remove(position);
+        notifyItemRemoved(position);
     }
 
     // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
         @BindView(R.id.product_image)
         ImageView productPicture;
+        private Callback callback;
 
-        public ViewHolder(View v) {
-            super(v);
+        ViewHolder(View itemView, Callback callback) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.callback = callback;
+        }
+
+        void bind(Context context, final int position, final Uri picture) {
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (callback != null) {
+                        callback.onItemClick(picture.toString(), position);
+                    }
+                }
+            });
+
+            if (picture.toString().isEmpty()) return;
+            productPicture.setVisibility(View.VISIBLE);
+            Picasso.with(context)
+                    .load(picture)
+                    .into(productPicture);
         }
     }
 
+    public interface Callback {
+        void onItemClick(String picture, int position);
+    }
 }
--- app/src/main/java/com/mtesitoo/fragment/AddProductPictureFragment.java
+++ app/src/main/java/com/mtesitoo/fragment/AddProductPictureFragment.java
@@ -1,15 +1,25 @@
 package com.mtesitoo.fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.mtesitoo.AbstractPermissionFragment;
+import com.mtesitoo.Constants;
 import com.mtesitoo.R;
 import com.mtesitoo.adapter.AddProductPicturesAdapter;
+import com.mtesitoo.model.ImageFile;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,10 +28,21 @@ import butterknife.ButterKnife;
  * Created by eduardodiaz on 18/10/2017.
  */
 
-public class AddProductPictureFragment extends Fragment {
+public class AddProductPictureFragment extends AbstractPermissionFragment {
 
     @BindView(R.id.pictures_recyclerview)
     RecyclerView picturesRecyclerView;
+
+    AddProductPicturesAdapter addProductPicturesAdapter;
+
+    private static final int SELECT_PICTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+
+    private int actionSelected = -1;
+    private ImageFile imageFile;
+
+    private boolean isNewImage = true;
+    private int selectedPosition = -1;
 
     public AddProductPictureFragment() {
 
@@ -40,7 +61,134 @@ public class AddProductPictureFragment extends Fragment {
     }
 
     private void populateScreen() {
-        picturesRecyclerView.setAdapter(new AddProductPicturesAdapter());
+        addProductPicturesAdapter = new AddProductPicturesAdapter(getContext(), new AddProductPicturesAdapter.Callback() {
+            @Override
+            public void onItemClick(final String picture, final int position) {
+
+                AddProductBottomSheetDialogFragment bottomSheetDialogFragment = AddProductBottomSheetDialogFragment.newInstance(picture.isEmpty());
+                bottomSheetDialogFragment.show(getFragmentManager(), bottomSheetDialogFragment.getTag());
+
+                isNewImage = picture.isEmpty();
+                selectedPosition = position;
+
+                bottomSheetDialogFragment.setCallback(new AddProductBottomSheetDialogFragment.Callback() {
+                    @Override
+                    public void optionSelected(int action) {
+                        switch (action) {
+                            case AddProductBottomSheetDialogFragment.ACTION_CAMERA:
+                                if (checkStoragePermission()) {
+                                    takePicture();
+                                } else {
+                                    actionSelected = REQUEST_IMAGE_CAPTURE;
+                                    //Don't have permissions at this point, so go ahead and request permissions
+                                    Toast.makeText(getActivity(), R.string.msg_permission_sorry, Toast.LENGTH_LONG)
+                                            .show();
+                                    requestPermission(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
+                                }
+                                break;
+                            case AddProductBottomSheetDialogFragment.ACTION_GALLERY:
+                                if (checkStoragePermission()) {
+                                    pickFromGallery();
+                                } else {
+                                    actionSelected = SELECT_PICTURE;
+                                    //Don't have permissions at this point, so go ahead and request permissions
+                                    Toast.makeText(getActivity(), R.string.msg_permission_sorry, Toast.LENGTH_LONG)
+                                            .show();
+                                    requestPermission(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
+                                }
+                                break;
+
+                            case AddProductBottomSheetDialogFragment.ACTION_REMOVE:
+                                addProductPicturesAdapter.removePicture(position);
+                                break;
+                        }
+                    }
+                });
+            }
+        });
+        picturesRecyclerView.setAdapter(addProductPicturesAdapter);
+
+        picturesRecyclerView.setHasFixedSize(true);
         picturesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+    }
+
+    private boolean checkStoragePermission() {
+        return super.isReady() || super.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    protected String[] getDesiredPermissions() {
+        //Note: Add permissions here if permissions must be granted at the load of screen.
+        // Otherwise go for granular approach to grant permissions as needed.
+
+        //return(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE});
+        return null;
+    }
+
+    @Override
+    protected void onPermissionDenied() {
+        Toast.makeText(getActivity(), R.string.msg_permission_sorry, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    protected void onReady(Bundle state) {
+        if (actionSelected == REQUEST_IMAGE_CAPTURE) {
+            takePicture();
+        } else if (actionSelected == SELECT_PICTURE) {
+            pickFromGallery();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK
+                && data != null && data.getData() != null) {
+
+            includeNewPicture(data.getData());
+
+        } else if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE) {
+
+            includeNewPicture(imageFile.getUri());
+        }
+    }
+
+    private void includeNewPicture(Uri pictureUri) {
+        if (isNewImage) {
+            addProductPicturesAdapter.insertPicture(selectedPosition, pictureUri);
+        } else {
+            addProductPicturesAdapter.updatePicture(selectedPosition, pictureUri);
+        }
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            ImageFile image = null;
+
+            try {
+                image = new ImageFile(getActivity());
+            } catch (Exception e) {
+                Log.d("IMAGE_CAPTURE", "Issue creating image file");
+            }
+
+            if (image != null) {
+                Uri imgUri = FileProvider.getUriForFile(getActivity(),
+                        Constants.FILE_PROVIDER,
+                        image);
+
+                imageFile = image;
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
 }
